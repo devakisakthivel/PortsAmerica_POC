7dc8d11b97e77f751ab61bf0e1eb840c3361cbd0
Chrisman <TChrisman@mymic.net>
2016-06-02 12:14:48 -0400

    First commit

---
+++ Assets/AmbulanceLights.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbulanceLights : MonoBehaviour {
+
+    
+    public Color firstColor, secondColor;
+    public bool onFirst;
+    Component[] lensFlares;
+    Component[] spotLight;
+    public float flashDelay;
+    
+	
+
+    void OnEnable()
+    {
+            spotLight = GetComponentsInChildren<Light>();
+            lensFlares = GetComponentsInChildren<LensFlare>();
+            StartCoroutine(EmergencyLights());
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+    IEnumerator EmergencyLights()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            yield return new WaitForSeconds(flashDelay);
+            foreach (LensFlare flare in lensFlares)
+            {
+                flare.enabled = onFirst;
+            }
+
+            foreach (Light lights in spotLight)
+            {
+                lights.enabled = onFirst;
+            }
+            GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", firstColor);
+            yield return new WaitForSeconds(flashDelay);
+            foreach (LensFlare flare in lensFlares)
+            {
+                flare.enabled = !onFirst;
+            }
+
+            foreach (Light lights in spotLight)
+            {
+                lights.enabled = !onFirst;
+            }
+            GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", secondColor);
+        }
+    }
+}
---
+++ Assets/Scripts/Warehouse_Scripts.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warehouse_Scripts : MonoBehaviour {
+
+    [Header("Box Objects")]
+    public Rigidbody Box1;
+    public Rigidbody Box2, Box3, Box4, BigBox1, BigBox2;
+
+    [Header("Audio Sources")]
+    public AudioSource WoodenBoxes_Aud;
+
+	void Awake()
+	{
+	}
+
+	void Start() 
+	{
+	}
+	
+	void Update()
+	{
+	}
+
+
+    public void AddForce()
+    {
+        //Remove freeze rotation from the boxes
+        Box1.freezeRotation = false;
+        Box2.freezeRotation = false;
+        Box3.freezeRotation = false;
+        Box4.freezeRotation = false;
+
+
+        //Add force to the boxes
+        Box1.AddForce(new Vector3(-400, 0, 0), ForceMode.Impulse);
+        Box2.AddForce(new Vector3(-700, 0, 0), ForceMode.Impulse);
+        Box3.AddForce(new Vector3(-700, 0, 0), ForceMode.Impulse);
+        Box4.AddForce(new Vector3(-400, 0, 0), ForceMode.Impulse);
+
+        BigBox1.AddForce(new Vector3(0, -200, 0), ForceMode.Impulse);
+        BigBox2.AddForce(new Vector3(0, -200, 0), ForceMode.Impulse);
+
+        //Play Wooden Boxes Falling Audio
+        WoodenBoxes_Aud.Play();
+    }
+}
